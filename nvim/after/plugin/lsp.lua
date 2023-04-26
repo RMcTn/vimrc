4b97d25a9080ea4080bc20739714e7da44606c39
Reece McTiernan <reece.mctiernan@immersivelabs.com>
2023-04-26 11:36:17 +0100

    Add more lsp keymaps

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -27,18 +27,17 @@ lsp.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
 	--- TODO(reece): 
-		-- nmap <silent> gi <Plug>(coc-implementation)
-		-- nmap <silent> gy <Plug>(coc-type-definition)
 		-- nmap <leader>qf  <Plug>(coc-fix-current)
-		-- nmap <silent> gi <Plug>(coc-implementation)
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+	vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+	vim.keymap.set("n", "gy", function() vim.lsp.buf.type_definition() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<c-j>", function() vim.diagnostic.goto_next() end, opts)
 	vim.keymap.set("n", "<c-k>", function() vim.diagnostic.goto_prev() end, opts)
 	vim.keymap.set("n", "<leader>ac", function() vim.lsp.buf.code_action() end, opts)
 	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 end)
 
 lsp.set_preferences({
