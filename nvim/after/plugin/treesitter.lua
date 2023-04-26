ba4a53fc0a194e7edee285dee0a5cbf6e4e28479
Reece McTiernan <reece.mctiernan@immersivelabs.com>
2023-04-26 11:09:57 +0100

    Disable gitcommit treesitter parser
    It wasn't working with the 'git commit -v' diff view, so just disabling
    for now instead of investigating

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -9,11 +9,14 @@ require'nvim-treesitter.configs'.setup {
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
+
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
   highlight = {
     enable = true,
+
+    disable = {"gitcommit"},
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
